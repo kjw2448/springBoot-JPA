43ab2d49ba9c4ed8d1d470fc9be1146a850ff299
kjw2448 <kjw2448@naver.com>
2021-07-21 15:09:32 +0900

    springBoot-JPA

---
+++ src/test/java/com/kim/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kim.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
