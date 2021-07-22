004c704a440fbdb43737c10ea13729b3dec577b2
kjw2448 <kjw2448@naver.com>
2021-07-22 15:42:45 +0900

    Exception 핸들러

--- src/main/java/com/kim/blog/handler/GlobalExceptionHandler.java
+++ src/main/java/com/kim/blog/handler/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package com.kim.blog.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kim.blog.dto.ResponseDto;
 
 @ControllerAdvice // 어디서 발생한 exception도 다 받기 위해
 @RestController
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(value=Exception.class)
-	public String handleArgumentException(IllegalArgumentException e) {
-		return "<h1>" + e.getMessage() + "</h1>";
+	public ResponseDto<String> handleArgumentException(Exception e) {
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(),  e.getMessage());
 	}
 }
