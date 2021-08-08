2da94b47c181e1880b9477ab69c4f9cc40c241c8
kjw2448 <kjw2448@naver.com>
2021-08-08 16:40:39 +0900

    글목록 페이징

--- src/main/java/com/kim/blog/controller/BoardController.java
+++ src/main/java/com/kim/blog/controller/BoardController.java
@@ -1,6 +1,9 @@
 package com.kim.blog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +18,9 @@ public class BoardController {
 	
 	// 컨트롤러에서 세션을 어떻게 찾는지?
 	@GetMapping({"/",""})
-	public String index(Model model) { 
+	public String index(Model model, @PageableDefault(size=3,direction=Sort.Direction.DESC) Pageable pageable) { 
 		// index.jsp 페이지로 boards가 model을 타고 넘어간다.
-		model.addAttribute("boards", boardService.글목록());
+		model.addAttribute("boards", boardService.글목록(pageable));
 		return "index"; // viewResolver 작동!! (prefix, suffix 포함)
 	}
 	
